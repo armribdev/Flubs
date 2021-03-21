9e902de5ff6ba0962be16782b2ac6a717af93c24
Armand Ribault <ribaultarmand@gmail.com>
2021-03-21 21:06:31 +0100

    Added spawner

---
+++ Assets/SpawnerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerController : MonoBehaviour
+{
+    private float spawningDeltaTime;
+
+    [SerializeField]
+    private int stock;
+
+    [SerializeField]
+    private GameObject prefab;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawningDeltaTime = 2.0f;
+        InvokeRepeating("SpawnFlub", .0f, spawningDeltaTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnFlub()
+    {
+        if (stock > 0)
+        {
+            Instantiate(prefab, transform.position, transform.rotation);
+            stock--;
+        }
+    }
+}
