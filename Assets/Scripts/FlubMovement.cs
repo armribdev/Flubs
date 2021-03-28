419b81d9273b7c3c816246ea3046df5bd0d20b98
Armand Ribault <ribaultarmand@gmail.com>
2021-03-28 20:04:55 +0200

    Code improvements

--- Assets/Scripts/FlubMovement.cs
+++ Assets/Scripts/FlubMovement.cs
@@ -3,47 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
-[RequireComponent(typeof(CapsuleCollider2D))]
 public class FlubMovement : MonoBehaviour
 {
+    enum Direction {Left , Right};
+    
     [SerializeField]
     private float moveSpeed;
-    private bool facingRight;
+    [SerializeField]
+    private float maxClimbing;
+    
+    private Direction direction;
 
     private Rigidbody2D rb;
     private CapsuleCollider2D cc;
     private Vector3 velocity = Vector3.zero;
-    // Start is called before the first frame update
     
     void Awake()
     {
-        facingRight = true;
+        direction = Direction.Right;
     }
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        cc = GetComponent<CapsuleCollider2D>();
     }
-
-    // Update is called once per frame
-    /*
-    void Update()
-    {
-        if (cc.)
-        float horizontalMovement = moveSpeed * Time.deltaTime;
-        if (!facingRight) horizontalMovement *= -1;
-        Move(horizontalMovement);
-    }*/
 
     void OnCollisionStay2D(Collision2D collision)
     {
         float horizontalMovement = moveSpeed * Time.deltaTime;
-        if (!facingRight) horizontalMovement *= -1;
+        if (direction == Direction.Left) horizontalMovement *= -1;
         Move(horizontalMovement);
     }
 
     private void Move(float horizontalMovement) {
         Vector3 target = new Vector2 (horizontalMovement, rb.velocity.y);
         rb.velocity = Vector3.SmoothDamp(rb.velocity, target, ref velocity, .05f);
+    }
+
+    private void Flip()
+    {
+        if (direction == Direction.Left) direction = Direction.Right;
+        else if (direction == Direction.Right) direction = Direction.Left;
     }
 }
