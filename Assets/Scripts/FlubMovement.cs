03b8214b6afd4cf33c1f818c3da0912552c1dcc4
Armand Ribault <ribaultarmand@gmail.com>
2021-03-28 19:41:45 +0200

    Flubs move automatically only when colliding

--- Assets/Scripts/FlubMovement.cs
+++ Assets/Scripts/FlubMovement.cs
@@ -3,26 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CapsuleCollider2D))]
 public class FlubMovement : MonoBehaviour
 {
     [SerializeField]
     private float moveSpeed;
+    private bool facingRight;
 
     private Rigidbody2D rb;
+    private CapsuleCollider2D cc;
     private Vector3 velocity = Vector3.zero;
     // Start is called before the first frame update
+    
+    void Awake()
+    {
+        facingRight = true;
+    }
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        cc = GetComponent<CapsuleCollider2D>();
     }
 
     // Update is called once per frame
+    /*
     void Update()
     {
-        float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        if (cc.)
+        float horizontalMovement = moveSpeed * Time.deltaTime;
+        if (!facingRight) horizontalMovement *= -1;
+        Move(horizontalMovement);
+    }*/
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        float horizontalMovement = moveSpeed * Time.deltaTime;
+        if (!facingRight) horizontalMovement *= -1;
         Move(horizontalMovement);
     }
-
 
     private void Move(float horizontalMovement) {
         Vector3 target = new Vector2 (horizontalMovement, rb.velocity.y);
