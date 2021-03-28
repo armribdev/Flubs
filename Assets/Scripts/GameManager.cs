50e18d4c0b72e9196e3a27b3f78627184944f5f2
Armand Ribault <ribaultarmand@gmail.com>
2021-03-28 19:01:21 +0200

    Added GameManager.cs

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    void Start()
+    {
+    }
+
+    public void PrintTest() {
+        Debug.Log("test!");
+    }
+}
