83251c5beac0a30750f4e911ee654a57f64188e4
Armand Ribault <ribaultarmand@gmail.com>
2021-04-14 15:00:31 +0200

    Added FlubSelector.cs

---
+++ Assets/FlubSelector.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class FlubSelector : MonoBehaviour
+{
+    private Camera cam;
+
+    private Vector3 startPos;
+    
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            startPos = cam.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        if (Input.GetMouseButtonUp(0)) {
+            Collider2D[] collider2DArray = Physics2D.OverlapAreaAll(startPos, cam.ScreenToWorldPoint(Input.mousePosition));
+            foreach (Collider2D collider2D in collider2DArray) {
+                Debug.Log(collider2D);
+            }
+        }
+    }
+}
