bdef19c2de405b0bf9b155b6f796395bd6d6a1c1
Lucas D'aquaro <luca.daquaro@gmail.com>
2021-04-27 21:57:04 +0200

    Menu + structure niveaux tutos

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Niveau1()
+    {
+		SceneManager.LoadScene(1);  // S'assurer que le niveau 1 est bien en position 1 dans le build
+	}
+
+	public void QuitGame()
+    {
+        Debug.Log("QUIT !");
+        Application.Quit();
+    }
+}
